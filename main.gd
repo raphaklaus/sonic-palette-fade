aaee06f9b7d04e6f76aecb9d4fcb0622ed604737
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-05-02 19:33:46 +0200

    Make it reusable

--- main.gd
+++ main.gd
@@ -1,31 +1,13 @@
-extends Sprite
+extends CanvasLayer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func init(transition_speed):
+	$AnimationPlayer.playback_speed = 2 * 0.5 / transition_speed
+	$Shader.set_size(get_viewport().size)
 
-var controlled_time
-var delta_controlled_time = 0
-var time_started
-var frames_count = 0
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	time_started = OS.get_ticks_msec()
+func fade_in():
+	print("fade in")
+	$AnimationPlayer.play("fade_in")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-#	controlled_time = OS.get_ticks_msec()
-	
-	frames_count += 0.015
-
-	if frames_count >+ 2:
-		frames_count = 0
-	
-#	if controlled_time >= 2:
-#		delta_controlled_time += controlled_time
-#		controlled_time -= delta_controlled_time
-
-#	print(frames_count)
-	material.set_shader_param("frames_count", frames_count)
+func fade_out():
+	$AnimationPlayer.play("fade_out")
